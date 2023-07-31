e7ae404ac9da793dc33e9bb82e7acc2493109252
Tadidivyaa <126139531+Tadidivyaa@users.noreply.github.com>
2023-07-31 20:45:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Emoji from './Emoji';
+
+function App() {
+  return (
+    <div className="App">
+      <Emoji/>
+    </div>
+  );
+}
+
+export default App;
